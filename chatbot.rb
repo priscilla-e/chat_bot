3ad22fc1883709457c0b98c28b678dd267184ffa
Priscilla Emasoga <priscillaemasoga@gmail.com>
2021-10-21 03:50:56 +0100

    initial commit

---
+++ chatbot.rb
@@ -0,0 +1,19 @@
+loop do
+  print '> '
+  user_input = gets.chomp
+  is_upcase = user_input.upcase
+  if user_input == 'BYE!'
+    break
+  else
+    if user_input == is_upcase then
+      puts "No, not since #{rand(1930...1950)}"
+    else
+      random = rand 3
+      if random < 2 then
+        puts 'What?'
+      else
+        puts 'HUH? Speak up sonny!'
+      end
+    end
+  end
+end
