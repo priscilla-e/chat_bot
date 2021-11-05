6085c116289de6d95c4bec7199d64ce527dc4df1
Priscilla Emasoga <priscillaemasoga@gmail.com>
2021-11-05 15:26:10 +0000

    added quotes and ignore feature

--- chatbot.rb
+++ chatbot.rb
@@ -1,17 +1,31 @@
 #A simple ChatBot program
 
+quotes = File.readlines "quotes.db"
+
 loop do
   print '> '
   user_input = gets.chomp
   is_upcase = user_input.upcase
   if user_input == 'BYE!'
+    shout_count = 1
+    while shout_count < 3 do
+      print '> '
+      user_input = gets.chomp
+      if user_input == 'BYE!' then
+        shout_count += 1
+      else
+        shout_count = 0
+      end
+    end
     break
+  elsif user_input.end_with?('?') then
+    puts "#{quotes.sample}"
   else
     if user_input == is_upcase then
       puts "No, not since #{rand(1930...1950)}"
     else
-      random = rand 3
-      if random < 2 then
+      random = rand 2
+      if random < 1 then
         puts 'What?'
       else
         puts 'HUH? Speak up sonny!'
